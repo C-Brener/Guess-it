ce35c6ec7244a127bb1fc82e3204ab679861cab6
brenermr <brenercaique0806@gmail.com>
2022-03-28 20:00:11 -0300

    Implementation Counter

--- app/src/main/java/com/example/android/guesstheword/screens/game/ui/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/ui/GameFragment.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.android.guesstheword.screens.game.ui
 
@@ -64,11 +49,20 @@ class GameFragment : Fragment() {
             binding.scoreText.text = scoreText.toString()
 
         })
-        viewModel.wordLiveData.observe(viewLifecycleOwner, Observer { nextWord->
+        viewModel.wordLiveData.observe(viewLifecycleOwner, Observer { nextWord ->
             binding.wordText.text = nextWord.toString()
 
-
         })
+        viewModel.eventGameFinish.observe(viewLifecycleOwner, Observer{
+            if (it){
+                val currentPoints = viewModel.scoreLiveData.value?:0
+                gameFinished(currentPoints)
+            }
+        })
+        viewModel.currentTime.observe(viewLifecycleOwner, Observer {
+            binding.timerText.text = viewModel.currentTime.value.toString()
+        })
+
         return binding.root
 
     }
@@ -81,9 +75,10 @@ class GameFragment : Fragment() {
     /**
      * Called when the game is finished
      */
-    private fun gameFinished() {
-        val action = GameFragmentDirections.actionGameToScore((viewModel.scoreLiveData ?:0) as Int)
+    private fun gameFinished(currentPoints:Int) {
+        val action = GameFragmentDirections.actionGameToScore(currentPoints)
         findNavController(this).navigate(action)
+        viewModel.onGameFinishComplete()
     }
 
     /**
--- app/src/main/java/com/example/android/guesstheword/screens/game/viewModel/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/viewModel/GameViewModel.kt
@@ -1,26 +1,60 @@
 package com.example.android.guesstheword.screens.game.viewModel
 
+import android.os.CountDownTimer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    
+    companion object {
+        // These represent different important times
+        // This is when the game is over
+        const val DONE = 0L
+
+        // This is the number of milliseconds in a second
+        const val ONE_SECOND = 1000L
+
+        // This is the total time of the game
+        const val COUNTDOWN_TIME = 60000L
+    }
+
+    val timer: CountDownTimer
+
 //    Live Data
 
     private var _wordLiveData = MutableLiveData<String>()
-    var wordLiveData : LiveData<String> = _wordLiveData
-    
+    var wordLiveData: LiveData<String> = _wordLiveData
+
     private var _scoreLiveData = MutableLiveData<Int>()
-    var scoreLiveData : LiveData<Int> = _scoreLiveData
-    
-    
+    val scoreLiveData: LiveData<Int> = _scoreLiveData
+
+    private var _currentTime = MutableLiveData<Long>()
+    var currentTime: LiveData<Long> = _currentTime
+
+    private var _eventGameFinish = MutableLiveData<Boolean>()
+    var eventGameFinish: LiveData<Boolean> = _eventGameFinish
+
+
     private lateinit var wordList: MutableList<String>
 
     init {
         resetList()
         nextWord()
         _scoreLiveData.value = 0
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished/ ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                _eventGameFinish.value = true
+            }
+
+        }
+
+        timer.start()
     }
 
 
@@ -54,10 +88,10 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-//            gameFinished()
-        } else {
-            _wordLiveData.value = wordList.removeAt(0)
+            resetList()
+
         }
+        _wordLiveData.value = wordList.removeAt(0)
 
     }
 
@@ -71,4 +105,12 @@ class GameViewModel : ViewModel() {
         nextWord()
     }
 
+    fun onGameFinishComplete() {
+        _eventGameFinish.value = false
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
+    }
 }
